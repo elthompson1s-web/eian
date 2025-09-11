1c8e485aed7fe3ec56a37ba367d9b2add00ed718
elthompson1s-web <elthompson1s@capetigers.com>
2025-09-11 16:16:14 +0000

    fixed errors and added calc.cpp

---
+++ code/calc/calc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int multiply(int number1, int number2) {
+    return(number1 * number2);
+} 
+
+
+int main() {  
+    int number1;
+    int number2;
+    int number3;
+    /* code */
+    return 0;
+}
