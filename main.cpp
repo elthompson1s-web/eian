288dad6c1704b6fd55c471eb13e0b15fb6e781e6
elthompson1s-web <elthompson1s@capetigers.com>
2025-09-18 15:57:03 +0000

    Added password

--- main.cpp
+++ main.cpp
@@ -17,29 +17,31 @@ int main() {
     int number1;
     int number2;
     int number3;
+    char io;
     char op;
     
-    std::cout << "whats your first number ";
-    std::cin >> number1;
-    std::cout << "whats your operator ";
-    std::cin >> op;
-    std::cout << "whats your second number ";
-    std::cin >> number2;
-    if (op == '*'){
-        number3 = multiply(number1, number2); 
-    }
-    if (op == '+'){
-        number3 = addition(number1, number2);
-    }
-    if (op == '-'){
-        number3 = subtract(number1, number2);
-    }
-    if (op == '/'){
-        number3 = divide(number1, number2);
-    }
     
-    std::cout << "your number was: " << number3;
-    
-    
+    do {
+        std::cout << "whats your first number ";
+        std::cin >> number1;
+        std::cout << "whats your operator ";
+        std::cin >> op;
+        std::cout << "whats your second number ";
+        std::cin >> number2;
 
-}   
+        switch(op) {
+            case '*': number3 = multiply(number1, number2); break;
+            case '+': number3 = addition(number1, number2); break;
+            case '-': number3 = subtract(number1, number2); break;
+            case '/': number3 = divide(number1, number2); break;
+            
+        }
+
+        std::cout << "your number was: " << number3 << '\n';
+        std::cout << "wanna restart? (y/n)";
+        std::cin >> io; std::cout << '\n';
+    } while (io != 'n' || 'N');
+    
+    }
+    
+   
